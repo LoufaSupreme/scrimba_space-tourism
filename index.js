2e4dacec162e8ad2841306a9940fdc8b06c9a79d
josh <josh.davis@live.ca>
2021-10-31 22:55:19 -0400

    improved styling for large sizes

--- index.js
+++ index.js
@@ -3,5 +3,10 @@ const primaryNavigation = document.querySelector("#primary-navigation");
 
 navToggle.addEventListener('click', () => {
   primaryNavigation.classList.toggle('open');
-  navToggle.classList.toggle('open');
+  
+  if (primaryNavigation.classList.contains('open')) {
+    navToggle.setAttribute('aria-expanded', true);
+  } else {
+    navToggle.setAttribute('aria-expanded', false);
+  }
 })
